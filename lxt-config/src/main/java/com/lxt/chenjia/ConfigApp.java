343defb3c2e11cee1ee2ce26b8a4c579c4d82fbc
chenjia <chenjia@n22.com.cn>
2018-07-24 23:14:45 +0800

    aa

--- lxt-config/src/main/java/com/lxt/chenjia/ConfigApp.java
+++ lxt-config/src/main/java/com/lxt/chenjia/ConfigApp.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApp extends SpringBootServletInitializer{
--- lxt-eureka/src/main/java/com/lxt/chenjia/eureka/EurekaApp.java
+++ lxt-eureka/src/main/java/com/lxt/chenjia/eureka/EurekaApp.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableEurekaClient
 public class EurekaApp extends SpringBootServletInitializer{
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
