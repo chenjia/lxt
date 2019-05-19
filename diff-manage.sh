520a8c82ff2d8edf25f1b6aa1efd005a91d040d9
chenjia <251746034@qq.com>
2019-05-19 17:10:07 +0800

    aa

---
+++ diff-manage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+file1=`wc -c lxt-manage-1.0-SNAPSHOT.jar | awk '{print $1}'`
+file2=`wc -c lxt-manage-1.0-SNAPSHOT.jar.old | awk '{print $1}'`
+if [[ $file1 == $file2 ]]
+then echo "00"
+else echo "99"
+fi
