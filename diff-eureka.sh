fefca2018a0d3da940e4e49e1ee6bd65637aa59e
chenjia <251746034@qq.com>
2019-05-20 09:09:43 +0800

    aa

--- diff-eureka.sh
+++ diff-eureka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 file1=`wc -c /home/chenjia/applications/lxt-eureka-1.0-SNAPSHOT.jar | awk '{print $1}'`
-file2=`wc -c target/lxt-eureka-1.0-SNAPSHOT.jar | awk '{print $1}'`
+file2=`wc -c lxt-eureka/target/lxt-eureka-1.0-SNAPSHOT.jar | awk '{print $1}'`
 if [[ $file1 == $file2 ]]
 then echo "00"
 else echo "99"
--- diff-gateway.sh
+++ diff-gateway.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 file1=`wc -c /home/chenjia/applications/lxt-gateway-1.0-SNAPSHOT.jar | awk '{print $1}'`
-file2=`wc -c target/lxt-gateway-1.0-SNAPSHOT.jar | awk '{print $1}'`
+file2=`wc -c lxt-gateway/target/lxt-gateway-1.0-SNAPSHOT.jar | awk '{print $1}'`
 if [[ $file1 == $file2 ]]
 then echo "00"
 else echo "99"
