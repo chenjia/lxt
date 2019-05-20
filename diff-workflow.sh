b69a25e65e5f43e4e12860e4f1d8499dd2674d20
chenjia <251746034@qq.com>
2019-05-20 10:23:03 +0800

    aa

--- diff-workflow.sh
+++ diff-workflow.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -il
+#!/bin/bash
 
 file1=`wc -c /home/chenjia/applications/lxt-workflow-1.0-SNAPSHOT.jar | awk '{print $1}'`
 file2=`wc -c lxt-workflow/target/lxt-workflow-1.0-SNAPSHOT.jar | awk '{print $1}'`
