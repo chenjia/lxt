218c35127fd24c9f385e26469583bf86b00aab58
chenjia <251746034@qq.com>
2019-05-20 08:56:41 +0800

    aa

---
+++ diff-push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+file1=`wc -c /home/chenjia/applications/lxt-push.war | awk '{print $1}'`
+file2=`wc -c lxt-push.war.old | awk '{print $1}'`
+if [[ $file1 == $file2 ]]
+then echo "00"
+else echo "99"
+fi
