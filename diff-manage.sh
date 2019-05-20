91b81a6a608d5071534a5a1568662d6377b2bbcb
chenjia <251746034@qq.com>
2019-05-20 09:06:38 +0800

    aa

--- diff-manage.sh
+++ diff-manage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 file1=`wc -c /home/chenjia/applications/lxt-manage-1.0-SNAPSHOT.jar | awk '{print $1}'`
-file2=`wc -c lxt-manage-1.0-SNAPSHOT.jar.old | awk '{print $1}'`
+file2=`wc -c target/lxt-manage-1.0-SNAPSHOT.jar | awk '{print $1}'`
 if [[ $file1 == $file2 ]]
 then echo "00"
 else echo "99"
--- diff-push.sh
+++ diff-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 file1=`wc -c /home/chenjia/applications/lxt-push.war | awk '{print $1}'`
-file2=`wc -c lxt-push.war.old | awk '{print $1}'`
+file2=`wc -c target/lxt-push-1.0-SNAPSHOT.war | awk '{print $1}'`
 if [[ $file1 == $file2 ]]
 then echo "00"
 else echo "99"
