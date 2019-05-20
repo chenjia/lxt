1a2a3787afa303ac41bcad368ba6479cbc134ecd
chenjia <251746034@qq.com>
2019-05-20 10:02:44 +0800

    aa

--- diff-gateway.sh
+++ diff-gateway.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -l
+#!/bin/bash -il
 file1=`wc -c /home/chenjia/applications/lxt-gateway-1.0-SNAPSHOT.jar | awk '{print $1}'`
 file2=`wc -c lxt-gateway/target/lxt-gateway-1.0-SNAPSHOT.jar | awk '{print $1}'`
 if [[ ${file1} == ${file2} ]]
--- diff-push.sh
+++ diff-push.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -l
+#!/bin/bash -il
 file1=`wc -c /home/chenjia/applications/lxt-push.war | awk '{print $1}'`
 file2=`wc -c lxt-push/target/lxt-push-1.0-SNAPSHOT.war | awk '{print $1}'`
 if [[ ${file1} == ${file2} ]]
