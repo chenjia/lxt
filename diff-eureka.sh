09e827abb4eb3b21819b53549b01c005454444f3
chenjia <251746034@qq.com>
2019-05-20 09:46:20 +0800

    aa

--- diff-eureka.sh
+++ diff-eureka.sh
@@ -2,6 +2,6 @@
 file1=`wc -c /home/chenjia/applications/lxt-eureka-1.0-SNAPSHOT.jar | awk '{print $1}'`
 file2=`wc -c lxt-eureka/target/lxt-eureka-1.0-SNAPSHOT.jar | awk '{print $1}'`
 if [[ ${file1} == ${file2} ]]
-then echo "00"
-else echo "99"
+then echo 00
+else echo 99
 fi
--- diff-workflow.sh
+++ diff-workflow.sh
@@ -2,6 +2,6 @@
 file1=`wc -c /home/chenjia/applications/lxt-workflow-1.0-SNAPSHOT.jar | awk '{print $1}'`
 file2=`wc -c lxt-workflow/target/lxt-workflow-1.0-SNAPSHOT.jar | awk '{print $1}'`
 if [[ ${file1} == ${file2} ]]
-then echo "00"
-else echo "99"
+then echo 00
+else echo 99
 fi
